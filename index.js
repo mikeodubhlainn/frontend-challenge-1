b0fe8f77fcaeeeb584b549eb2dcbd57e67db39e3
mikeodubhlainn <119634574+mikeodubhlainn@users.noreply.github.com>
2022-12-26 20:57:00 +0000

    Adding Javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+class Like extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div><p>My first paragraph</p></div>
+        )}
+}
+
+const domContainer = document.getElementById("app");
+const root = ReactDOM.createRoot(domContainer);
+root.render(<Like />);
