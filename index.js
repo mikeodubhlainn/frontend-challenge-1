88092228b8c551b961570e8ca7d51686c3a72104
mikeodubhlainn <119634574+mikeodubhlainn@users.noreply.github.com>
2022-12-26 23:30:47 +0000

    dj

--- index.js
+++ index.js
@@ -8,18 +8,9 @@ class InfoBox extends React.Component {
   render() {
     return (
         <div className="infobox">
-        <h1>Personal info</h1>
-        <h2>Please provide your name, email address, and phone number.</h2>
-        <form>
-            <h3>Name</h3>
-            <input type="text"></input>
-            <h3>Email Address</h3>
-            <input type="text"></input>
-            <h3>Phone Number</h3>
-            <input type="text"></input>
-            <br/>
-            <button type="submit">Next Step</button>
-        </form>
+        <div class="row">
+            <button class="btn btn-primary">Add task</button>
+        </div>
         </div>
   )}
 }
