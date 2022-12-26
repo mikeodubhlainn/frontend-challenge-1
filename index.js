d940ddf6e1276520446dd6fc7de45e27d915fd33
mikeodubhlainn <119634574+mikeodubhlainn@users.noreply.github.com>
2022-12-26 23:09:46 +0000

    hkdj

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ function InfoBox() {
 
   return (
     <div className="infobox">
-    <h1>App is working</h1>
+    <h1>Personal info</h1>
+    <h2>Please provide your name, email address, and phone number.</h2>
+    <form>
+        <h3>Name</h3>
+        <input type="text"></input>
+        <h3>Email Address</h3>
+        <input type="text"></input>
+        <h3>Phone Number</h3>
+        <input type="text"></input>
+        <button type="submit"></button>
+    </form>
     </div>
   )
 }
