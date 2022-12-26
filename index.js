5a1b524e7f5341844d325b6c2a940904ba74d72e
mikeodubhlainn <119634574+mikeodubhlainn@users.noreply.github.com>
2022-12-26 23:11:29 +0000

    dshjk

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 'use strict';
 
-function InfoBox() {
+class InfoBox extends React.Component {
 
-  return (
-    <div className="infobox">
-    <h1>Personal info</h1>
-    <h2>Please provide your name, email address, and phone number.</h2>
-    <form>
-        <h3>Name</h3>
-        <input type="text"></input>
-        <h3>Email Address</h3>
-        <input type="text"></input>
-        <h3>Phone Number</h3>
-        <input type="text"></input>
-        <button type="submit"></button>
-    </form>
-    </div>
-  )
+  render() {
+    constructor(props) {
+        super(props)
+    }
+
+    return (
+        <div className="infobox">
+        <h1>Personal info</h1>
+        <h2>Please provide your name, email address, and phone number.</h2>
+        <form>
+            <h3>Name</h3>
+            <input type="text"></input>
+            <h3>Email Address</h3>
+            <input type="text"></input>
+            <h3>Phone Number</h3>
+            <input type="text"></input>
+            <button type="submit">Next Step</button>
+        </form>
+        </div>
+  )}
 }
 
 const rootNode = document.getElementById('info-box-root');
